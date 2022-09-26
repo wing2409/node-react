0720921136ada63b09f3c9aa48bed89f88a73b52
nonjara <78718681+nonjara@users.noreply.github.com>
2022-09-26 16:49:38 +0900

    model add

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000
+
+mongoose.connect(
+  'mongodb+srv://test:1234@cluster0.gax5u.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+  },
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
