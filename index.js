e6059bb1c68315d5cfc3fe2e9dd00caf33c73edb
nonjara <78718681+nonjara@users.noreply.github.com>
2022-09-27 10:41:26 +0900

    commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 mongoose.connect(
   'mongodb+srv://test:1234@cluster0.gax5u.mongodb.net/?retryWrites=true&w=majority',
@@ -11,10 +16,17 @@ mongoose.connect(
 )
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!')
 })
 
-app.post('/register', (req, res) => {})
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({ success: true })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
